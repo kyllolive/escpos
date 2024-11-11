be144bfe348b46041c79239141dc191601f7c024
kyllolive <kyle.cuizon4@gmail.com>
2024-11-11 11:45:22 +0800

    fix lint

--- src/api/printToEpson.ts
+++ src/api/printToEpson.ts
@@ -1,34 +1,45 @@
-import express from "express";
-import { Printer, Image } from "@node-escpos/core";
-import USB from "@node-escpos/usb-adapter";
-import { join } from "path";
+import { Printer } from '@node-escpos/core';
+import USB from '@node-escpos/usb-adapter';
+import express from 'express';
 
 const router = express.Router();
 
-router.post<{}, any>("/", (req, res) => {
+
+router.get<{}, any>('/check-status', (req, res) => {
+  const device = new USB();
+  device.open(function (err) {
+    if (err) {
+      res.status(500).json({ message: 'Printer not connected' });
+    } else {
+      res.json({ message: 'Printer connected' });
+    }
+  });
+});
+
+router.post<{}, any>('/', (req, res) => {
   const { body } = req;
-  console.log("body", body);
+  console.log('body', body);
   const device = new USB();
 
   device.open(async function (err) {
     if (err) {
-      console.error("Error opening device:", err);
-      res.status(500).json({ message: "Error opening device" });
+      console.error('Error opening device:', err);
+      res.status(500).json({ message: 'Error opening device' });
       return;
     }
 
-    const options = { encoding: "UTF-8" };
+    const options = { encoding: 'UTF-8' };
 
     let printer = new Printer(device, options);
 
     try {
-      printer.font("a").text(body.message);
+      printer.font('a').text(body.message);
       //printer.cut();
       await printer.close();
-      res.json({ message: "Printed" });
+      res.json({ message: 'Printed' });
     } catch (error) {
-      console.error("Error printing:", error);
-      res.status(500).json({ message: "Error printing" });
+      console.error('Error printing:', error);
+      res.status(500).json({ message: 'Error printing' });
     } finally {
       device.close();
     }
